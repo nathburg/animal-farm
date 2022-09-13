7ca3d2ee8696f11746a17ab966a5ae7531a143f8
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-12 17:49:14 -0700

    styling of animal names

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -5,7 +5,7 @@ export default function Animal(props) {
   return (
     <div className="animal" style={{ top: props.top, left: props.left, backgroundImage: `url(${props.name}.svg)` }}>
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`}></img>
-      <div>{props.name}</div> 
+      <div className="name">{props.name}</div> 
       <div>{props.says}</div>
     </div>
   );
