1f1a3205a29ce8c684bbee5167aeb6fc4b292268
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-12 17:42:36 -0700

    images show up

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,7 +1,12 @@
+import './Animal.css';
+
 export default function Animal(props) {
+  console.log(props.top);
   return (
-    <div className="animal-card">
-        Name: {props.name} Type: {props.type}
+    <div className="animal" style={{ top: props.top, left: props.left, backgroundImage: `url(${props.name}.svg)` }}>
+      <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`}></img>
+      <div>{props.name}</div> 
+      <div>{props.says}</div>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { animals } from '../../data';
 import Animal from '../Animal/Animal';
 import './Main.css';
+import background from '../../background.png'; 
 
 export default function Main() {
-  return <main>
+  return <main style={{ backgroundImage: `url(${background})` }}>
+    
     {animals.map((animal) => (
-      <Animal key={animal.name} name={animal.name} type={animal.type} />
+      <Animal key={animal.name}
+        name={animal.name}
+        type={animal.type}
+        top={animal.top}
+        left={animal.left}
+        says={animal.says}
+      />
     ))}    
   </main>;
 }
