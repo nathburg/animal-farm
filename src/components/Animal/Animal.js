79244bd59d36c01efd219911689847e493278bed
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-12 18:01:53 -0700

    netlify error fix

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,7 +1,6 @@
 import './Animal.css';
 
 export default function Animal(props) {
-  console.log(props.top);
   return (
     <div className="animal" style={{ top: props.top, left: props.left, backgroundImage: `url(${props.name}.svg)` }}>
       <img src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`}></img>
